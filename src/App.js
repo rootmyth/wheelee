196fed76a3e49f0ce97dde3f335d462ccfbcbf44
rootmyth <rootmyth@gmail.com>
2022-06-20 15:59:02 -0500

    modified App.js to display one of two Routes dependent on whether a user is logged in or not

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import UserInterface from './Login/UserInterface';
+import Authenticate from './Login/Authenticate';
 
-function App() {
+const App = () => {
+
+  const user = parseInt(localStorage.getItem("wheelee_user"))
+
   return (
     <div className="App">
-      
+      {user ? <UserInterface/> : <Authenticate/>}
     </div>
   );
 }
