76d052798c0df92fbdb297b1f1fccdd1e75708d9
rootmyth <rootmyth@gmail.com>
2022-06-20 16:31:22 -0500

    Main routing for app when a user is logged in, the ShiftViews Route will always be the default

---
+++ src/AppViews.js
@@ -0,0 +1,27 @@
+import React, {useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import "./AppViews.css"
+import Information from "./Information/Information";
+import Repair from "./Repair/Repair";
+import ShiftViews from "./Shift/ShiftViews"
+import Contact from "./Contact/Contact";
+import Profile from "./Profile/Profile";
+import Login from "./Login/Authenticate";
+
+const AppViews = () => {
+
+    return (
+        <main className="AppViews">
+            <Routes>
+                <Route path="/information" element={<Information/>}/>
+                <Route path="/repair" element={<Repair/>}/>
+                <Route exact path="*" element={<ShiftViews/>}/>
+                <Route path="/contact" element={<Contact/>}/>
+                <Route path="/profile" element={<Profile/>}/>
+                <Route path="/login" element={<Login/>}/>
+            </Routes>
+        </main>
+    )
+}
+
+export default AppViews;
