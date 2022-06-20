c417a30e5d291d524f726d66f36a8b1a12d41388
rootmyth <rootmyth@gmail.com>
2022-06-20 16:32:45 -0500

    Empty contact view

---
+++ src/Contact/Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Contact = () => {
+    return (
+        <>
+            <h2>
+                Contacts
+            </h2>
+        </>
+    )
+}
+
+export default Contact;
