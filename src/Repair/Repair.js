1b97e3133edaec115db5483c49b75f1038c1fe29
rootmyth <rootmyth@gmail.com>
2022-06-22 23:42:41 -0500

    Repair.js introduces several states that determines the appearance of <ViewSelector/> and sends down repairRequest and Pedicab arrays as props to the different views.  There are also three functions that send data upstream from child components to set the states.  The return statement delivers the navigational view selector, a toggleable form to add a reapir (has yet to be built), and the three components that display the repairs in the DOM.

--- src/Repair/Repair.js
+++ src/Repair/Repair.js
@@ -1,13 +1,41 @@
 import React, { useState, useEffect } from "react"
+import ViewSelector from "./ViewSelector"
+import CabView from "./CabView"
+import TypeView from "./TypeView"
+import CompleteView from "./CompleteView"
 
 const Repair = () => {
 
     const [repairView, setRepairView] = useState(1)
+    const [addView, setAddView ] = useState(false)
     const [repairs, setRepairs] = useState ([])
+    const [pedicabs, setPedicabs] = useState([])
+
+    const sendViewNumberUpstream = (viewNumber) => {
+        setRepairView(viewNumber)
+        
+    }
+
+    const sendToggledAvailability = (toggleValue, cabNumber) => {
+        const cabs = [...pedicabs]
+        cabs[cabNumber - 1].isAvailable = toggleValue
+        setPedicabs(cabs)
+    }
 
     useEffect(
         () => {
-            fetch("http://localhost:8088/repairRequests?_expand=user")
+            fetch("http://localhost:8088/pedicabs")
+            .then(res => res.json())
+            .then((pedicabArray) => {
+                setPedicabs(pedicabArray)
+            })
+        },
+        []
+    )
+
+    useEffect(
+        () => {
+            fetch("http://localhost:8088/repairRequests?_expand=user&_expand=repairType")
             .then(res => res.json())
             .then((requestArray) => {
                 setRepairs(requestArray)
@@ -16,15 +44,33 @@ const Repair = () => {
         []
     )
 
+    const sendUpdatedRepairArrayUpstream = (repairId) => {
+        const repairArray = [...repairs]
+        const targetRepair = repairArray.find(repair => repair.id === repairId)
+        const repairIndex = repairArray.indexOf(targetRepair)
+        repairArray[repairIndex].isComplete = true
+        setRepairs(repairArray)
+    }
+
     return (
         <article className="Repair">
             <section className="Repair__viewSelector">
-                <ViewSelector />
+                <ViewSelector viewSelection={sendViewNumberUpstream}/>
+                {addView ? <h1>Add Repair</h1> : null}
             </section>
-            <section>
-            {repairs.map(repair => {
-                return <h1 key={repair.id}>{repair.description}</h1>
-            })}    
+            <section className="Repair__viewRender">
+                    {repairView === 1 ? 
+                        <CabView 
+                            cabs={pedicabs} 
+                            repairs={repairs} 
+                            availableCabChange={sendToggledAvailability} 
+                            repairListChange={sendUpdatedRepairArrayUpstream}
+                        />
+                    : 
+                        null
+                    }
+                    {repairView === 2 ? <TypeView repairs={repairs}/> : null}
+                    {repairView === 3 ? <CompleteView repairs={repairs}/> : null}
             </section>
         </article>
     )
