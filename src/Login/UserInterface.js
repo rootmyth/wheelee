b87447dce46208ca739bdab361ae64f00c70ed82
rootmyth <rootmyth@gmail.com>
2022-06-22 23:34:23 -0500

    Component now utilizes useState and useEffect to send props to child components.  The shift icon in the Nav will change color if the user is active, and the link will take the user to </ActiveShift or </Shift depending.  I also wrote a function as a prop that changes this state when a child component input affects this user's key-value pair.

--- src/Login/UserInterface.js
+++ src/Login/UserInterface.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Nav from "../Nav/Nav";
 import AppViews from "../AppViews";
 
 const UserInterface = () => {
+
+    const localUser = parseInt(localStorage.getItem("wheelee_user"))
+    const [currentUser, setCurrentUser] = useState({})
+
+    useEffect(
+        () => {
+            fetch("http://localhost:8088/users")
+            .then(res => res.json())
+            .then((userArray) => {
+                const user = userArray.find(user => user.id === localUser)
+                setCurrentUser(user)
+            })
+        },
+        [localUser]
+    )
+
+    const sendUserStatusUpstream = (userStatus) => {
+        const setStatus = {...currentUser}
+        setStatus.isActive = userStatus
+        setCurrentUser(setStatus)
+    }
+
     return (
         <>
-            <Nav/>
-            <AppViews/>
+            <Nav user={currentUser}/>
+            <AppViews user={currentUser} userStatusData={sendUserStatusUpstream}/>
         </>
     )
 }
