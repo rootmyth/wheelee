bbe6af9f8ba95d4f8db9eefa9948810f6c6b8a42
rootmyth <rootmyth@gmail.com>
2022-06-23 01:35:03 -0500

    imported useState and useEffect, considered using the fetched data in <UserInterface/> parent element but no reason to fetch the users until I want to display them.  Always fetch as late as you can

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Contact = () => {
+
+    const [users, setUsers] = useState([])
+
+    useEffect(
+        () => {
+            fetch("http://localhost:8088/users")
+            .then (res => res.json())
+            .then((userArray) => {
+                setUsers(userArray)
+            })
+        }
+    )
+
     return (
-        <>
-            <h2>
-                Contacts
-            </h2>
-        </>
+        <article className="Contact">
+            
+        </article>
     )
 }
 
