ddc54303c8734f1873f7be462ef808595ed6787e
rootmyth <rootmyth@gmail.com>
2022-06-22 23:46:30 -0500

    ViewSelector is a few <h1>s that set the view state for the Parent component and change appearence with every selection.  These navigate between repair views.

---
+++ src/Repair/ViewSelector.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import "./ViewSelector.css"
+
+const ViewSelector = (props) => {
+
+    const [viewState, setViewState] = useState(1)
+
+    return (
+            <div className="ViewSelector">
+                <div
+                    className={viewState === 1 ? "ViewSelector__cabs ViewSelector__element active" : "ViewSelector__cabs ViewSelector__element"}
+                    onClick={
+                        () => {
+                            setViewState(1)
+                            props.viewSelection(1)
+                        }
+                    }>
+                    <h1>Pedicabs</h1>
+                </div>
+                <div 
+                    className={viewState === 2 ? "ViewSelector__types ViewSelector__element active" : "ViewSelector__types ViewSelector__element"}
+                    onClick={
+                        () => {
+                            setViewState(2)
+                            props.viewSelection(2)
+                        }
+                    }>
+                    <h1>Repair Types</h1>
+                </div>
+                <div 
+                    className={viewState === 3 ? "ViewSelector__complete ViewSelector__element active" : "ViewSelector__complete ViewSelector__element"}
+                    onClick={
+                        () => {
+                            setViewState(3)
+                            props.viewSelection(3)
+                        }
+                    }>
+                    <h1>Complete</h1>
+                </div>
+                <div className="ViewSelector__add ViewSelector__element">
+                    <h1>Add</h1>
+                </div>
+            </div>
+            
+    )
+}
+
+export default ViewSelector
