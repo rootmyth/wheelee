12ef0ad4a2b413f378d6b37ba1dc276ad17a084d
rootmyth <rootmyth@gmail.com>
2022-06-23 18:52:55 -0500

    Contact component build minus option for manager to delete employees.  Relativly 'dumb' component that changes card style slightly depending on whether a user is active or not.  Could use some styling help when card info wraps under picture, it just doesn't look spectacular.

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,8 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react"
+import "./Contact.css"
 
 const Contact = () => {
 
     const [users, setUsers] = useState([])
+
+    const localUserId = parseInt(localStorage.getItem("wheelee_user"))
+    const localUserObj = users.find(user => user.id === localUserId)
+
+
+    const sortUsers = (userArray) => {
+        const sortActiveUsersToTop = (user) => {
+            if (user.isActive) {
+                return -1
+            }
+            if (!user.isActive) {
+                return 1
+            }
+        }
+        return userArray.sort(sortActiveUsersToTop)
+    }
+    const sortedUsers = sortUsers(users)
 
     useEffect(
         () => {
@@ -11,12 +29,45 @@ const Contact = () => {
             .then((userArray) => {
                 setUsers(userArray)
             })
-        }
+        },
+        []
     )
 
     return (
         <article className="Contact">
-            
+            {sortedUsers.map(user => {
+                if (user.id !== localUserId) {
+                    return (
+                        <section key={`contact--${user.id}`} className="Contact__card">
+                            <div className="Contact__card__image">
+                                <img
+                                    className={user.isActive ? "Contact__card__image__URL--active" : "Contact__card__image__URL"} src={user.imageURL} alt={`${user.name}'s face`} title={`${user.name}'s face`}/>
+                            </div>
+                            <div className="Contact__card__info">
+                                <div className="Contact__card__info__name">
+                                    {user.name}
+                                    <div className={`Contact__card__info__name__status${user.isActive ? "--active" : ""}`}>{user.isActive ? "Riding Now" : "Not Riding"}</div>
+                                </div>
+                                <div className="Contact__card__info__title">
+                                    {user.isMechanic ? "mechanic" : ""}
+                                    {user.isManager ? " | manager" : ""}
+                                </div>
+                                <div className="Contact__card__info__bio">
+                                    {user.bio}
+                                </div>
+                                <div className="Contact__card__info__methods">
+                                    <div className="Contact__card__info__methods__phone">
+                                        {user.phoneNumber}
+                                    </div>
+                                    <div className="Contact__card__info__methods__email">
+                                        {user.email}
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    )
+                }
+            })}
         </article>
     )
 }
