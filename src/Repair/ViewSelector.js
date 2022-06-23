af0b336ca34dea585f5602ca43b1f7dc12a89dc9
rootmyth <rootmyth@gmail.com>
2022-06-23 01:42:59 -0500

    Added addState for add repair field.

--- src/Repair/ViewSelector.js
+++ src/Repair/ViewSelector.js
@@ -4,40 +4,52 @@ import "./ViewSelector.css"
 const ViewSelector = (props) => {
 
     const [viewState, setViewState] = useState(1)
+    const [addState, setAddState] = useState(false)
 
     return (
-            <div className="ViewSelector">
+            <div className="ViewContainer">
+                <div className="ViewSelector">
+                    <div
+                        className={viewState === 1 ? "ViewSelector__cabs ViewSelector__element active" : "ViewSelector__cabs ViewSelector__element"}
+                        onClick={
+                            () => {
+                                setViewState(1)
+                                props.viewSelection(1)
+                            }
+                        }>
+                        <h1>Pedicabs</h1>
+                    </div>
+                    <div 
+                        className={viewState === 2 ? "ViewSelector__types ViewSelector__element active" : "ViewSelector__types ViewSelector__element"}
+                        onClick={
+                            () => {
+                                setViewState(2)
+                                props.viewSelection(2)
+                            }
+                        }>
+                        <h1>Repair Types</h1>
+                    </div>
+                    <div 
+                        className={viewState === 3 ? "ViewSelector__complete ViewSelector__element active" : "ViewSelector__complete ViewSelector__element"}
+                        onClick={
+                            () => {
+                                setViewState(3)
+                                props.viewSelection(3)
+                            }
+                        }
+                    >
+                        <h1>Complete</h1>
+                    </div>
+                </div>
                 <div
-                    className={viewState === 1 ? "ViewSelector__cabs ViewSelector__element active" : "ViewSelector__cabs ViewSelector__element"}
+                    className={addState ? "ViewSelector__add ViewSelector__element active__add" : "ViewSelector__add ViewSelector__element"}
                     onClick={
                         () => {
-                            setViewState(1)
-                            props.viewSelection(1)
+                            setAddState(!addState)
+                            props.toggleAdd()
                         }
-                    }>
-                    <h1>Pedicabs</h1>
-                </div>
-                <div 
-                    className={viewState === 2 ? "ViewSelector__types ViewSelector__element active" : "ViewSelector__types ViewSelector__element"}
-                    onClick={
-                        () => {
-                            setViewState(2)
-                            props.viewSelection(2)
-                        }
-                    }>
-                    <h1>Repair Types</h1>
-                </div>
-                <div 
-                    className={viewState === 3 ? "ViewSelector__complete ViewSelector__element active" : "ViewSelector__complete ViewSelector__element"}
-                    onClick={
-                        () => {
-                            setViewState(3)
-                            props.viewSelection(3)
-                        }
-                    }>
-                    <h1>Complete</h1>
-                </div>
-                <div className="ViewSelector__add ViewSelector__element">
+                    }
+                >
                     <h1>Add</h1>
                 </div>
             </div>
