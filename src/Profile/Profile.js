eb4ae8614103f887d1bbc92956fc3510fe5d1a53
rootmyth <rootmyth@gmail.com>
2022-06-24 16:18:16 -0500

    Created edit info button that toggles state to/from edit view.  Set up sections for profile information

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Profile.css"
 
-const Profile = () => {
+const Profile = (props) => {
+
+    const currentUser = props.localUser
+
+    const [editView, setEditView] = useState(false)
+
     return (
-        <>
-            <h2>
-                Profile
-            </h2>
-        </>
+        <article className="Profile">
+            <section className="Profile__image">
+
+            </section>
+            <section className="Profile__editBtn">
+                <div
+                    className={`Profile__editBtn__toggle${editView ? "--active" : ""}`}
+                    onClick={
+                        () => {
+                            setEditView(!editView)
+                        }
+                    }
+                >
+                    {editView ? "Save Changes" : "Edit Profile"}
+                </div>
+            </section>
+            <section className="Profile__bio">
+                
+            </section>
+            <section className="Profile__contact">
+                <div className="Profile__contact__phone">
+
+                </div>
+                <div className="Profile__contact__email">
+                    
+                </div>
+            </section>
+        </article>
     )
 }
 
