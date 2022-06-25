e6f6eb182776fa9b429a1514df9659e280cca9ff
rootmyth <rootmyth@gmail.com>
2022-06-25 02:34:07 -0500

    Built Profile component.  All elements in this component (user image, name, etc.) are editable when the user toggles an edit profile button.  Working as needed...

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -6,11 +6,110 @@ const Profile = (props) => {
     const currentUser = props.localUser
 
     const [editView, setEditView] = useState(false)
+    const [newProfileValue, setNewProfileValue] = useState(currentUser)
+    const [currentProfileValues, setCurrentProfileValues] = useState(currentUser)
 
     return (
         <article className="Profile">
             <section className="Profile__image">
-
+                <img className="Profile__image__img" src={currentUser.imageURL} alt={`${currentUser.name}'s face`} title={`${currentUser.name}'s face`}/>
+            </section>
+            <section className={`Profile__imageURL ${editView ? "" : "imageURL--inactive"}`}>
+                {editView ?
+                    <input
+                    className="Profile__imageURL__field"
+                    name="profileImageURL"
+                    type="text"
+                    value={`${newProfileValue.imageURL}`}
+                    onChange={
+                        (e) => {
+                            const copy = {...newProfileValue}
+                            copy.imageURL = e.target.value
+                            setNewProfileValue(copy)
+                        }
+                    }
+                />
+                :
+                    "Hello, I'm..."
+                }
+            </section>
+            <section className={`Profile__name Profile__element ${editView ? "" : "name--inactive"}`}>
+                {editView ?
+                        <input
+                        className="Profile__name__field"
+                        name="profileName"
+                        type="text"
+                        value={`${newProfileValue.name}`}
+                        onChange={
+                            (e) => {
+                                const copy = {...newProfileValue}
+                                copy.name = e.target.value
+                                setNewProfileValue(copy)
+                            }
+                        }
+                    />
+                    :
+                        currentUser.name
+                    }
+            </section>
+            <section className={`Profile__bio${editView ? "--active" : ""} Profile__element`}>
+            {editView ?
+                    <input
+                    className="Profile__bio__field"
+                    name="profileBio"
+                    maxLength="60"
+                    value={`${newProfileValue.bio}`}
+                    onChange={
+                        (e) => {
+                            const copy = {...newProfileValue}
+                            copy.bio = e.target.value
+                            setNewProfileValue(copy)
+                        }
+                    }
+                />
+                :
+                    currentUser.bio
+                }
+            </section>
+            <section className="Profile__contact Profile__element">
+                <div className={`Profile__contact__phone Profile__element ${editView ? "" : "phone--inactive"}`}>
+                {editView ?
+                    <input
+                    className="Profile__contact__phone__field"
+                    name="profilePhone"
+                    maxLength="15"
+                    value={`${newProfileValue.phoneNumber}`}
+                    onChange={
+                        (e) => {
+                            const copy = {...newProfileValue}
+                            copy.phoneNumber = e.target.value
+                            setNewProfileValue(copy)
+                        }
+                    }
+                />
+                :
+                    currentUser.phoneNumber
+                }
+                </div>
+                <div className={`Profile__contact__email Profile__element ${editView ? "" : "email--inactive"}`}>
+                {editView ?
+                    <input
+                    className="Profile__contact__email__field"
+                    name="profileBio"
+                    maxLength="60"
+                    value={`${newProfileValue.email}`}
+                    onChange={
+                        (e) => {
+                            const copy = {...newProfileValue}
+                            copy.email = e.target.value
+                            setNewProfileValue(copy)
+                        }
+                    }
+                />
+                :
+                    currentUser.email
+                }
+                </div>
             </section>
             <section className="Profile__editBtn">
                 <div
@@ -18,21 +117,11 @@ const Profile = (props) => {
                     onClick={
                         () => {
                             setEditView(!editView)
+                            
                         }
                     }
                 >
                     {editView ? "Save Changes" : "Edit Profile"}
-                </div>
-            </section>
-            <section className="Profile__bio">
-                
-            </section>
-            <section className="Profile__contact">
-                <div className="Profile__contact__phone">
-
-                </div>
-                <div className="Profile__contact__email">
-                    
                 </div>
             </section>
         </article>
