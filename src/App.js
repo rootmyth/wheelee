3ad84b68cf331e0a6d1d35f597dba4a7e0615f4f
rootmyth <rootmyth@gmail.com>
2022-07-07 16:22:34 -0500

    Removed UserInterface component as this was not in the correct location.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
-import UserInterface from './Login/UserInterface';
 import Authenticate from './Login/Authenticate';
 
 const App = () => {
 
-  const user = parseInt(localStorage.getItem("wheelee_user"))
-
   return (
     <div className="App">
-      {user ? <UserInterface/> : <Authenticate/>}
+      <Authenticate/>
     </div>
   );
 }
